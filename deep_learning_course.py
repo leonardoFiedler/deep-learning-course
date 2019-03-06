35d980d7e272b804f60290341e8c8ac967521445
Leonardo Fiedler <leonardo.fiedler.96@gmail.com>
2019-03-05 23:54:25 -0300

    Breast Cancer
    Fim da seção sobre breast cancer.Implementação de salvar e carregar a rede neural.

--- deep_learning_course.py
+++ deep_learning_course.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 
 # step Function = 
 # Relu: 0 ... +infinito
@@ -22,6 +23,19 @@ def softmaxFunction(x):
     ex = np.exp(x)
     return ex / ex.sum()
 
+def sigmoid(x):
+  return 1 / (1 + math.exp(-x))
+
 
 valores = [5.0, 2.0, 1.3]
-softmaxFunction(valores)
+sigmoid(2.1)
+
+mae = np.sum(np.absolute((imageB.astype("float") - imageA.astype("float")))
+
+
+(1 - 0,300)^2 = 0,49
+(1 - 0,890)^2 = 0,0121
+(0 - 0,20)^2 = 0,04
+(0 - 0.320)^2 = 0,1024
+
+0,49 + 0,0121 + 0,04 + 0,1024 = 0,6445 / 4 = 0,16
