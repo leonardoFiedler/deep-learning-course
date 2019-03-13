e62ad72f6ea7e45e568211e6042458642f3912e1
Leonardo Fiedler <leonardo.fiedler@teclogica.com.br>
2019-03-13 12:45:08 -0300

    Iris Simples, cruzada e breast cancer carregar.
    Adicionado breast cancer cruzada.
    Alterações na Iris simples e carregar de breast cancer.
    
    Change-Id: Icbe89875220c3a4dfd61f8bd66147b4834bfb8ba

--- iris/iris_simples.py
+++ iris/iris_simples.py
@@ -2,13 +2,22 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from keras.models import Sequential
 from keras.layers import Dense
+from sklearn.preprocessing import LabelEncoder #Transforma de atributo categórico para atributo numérico
+from keras.utils import np_utils
 
 base = pd.read_csv('iris.csv')
 previsores = base.iloc[:, 0:4].values
 classe = base.iloc[:, 4].values
+labelencoder = LabelEncoder()
 
-
-previsores_treinamento, previsores_teste, classe_treinamento, classe_teste = train_test_split(previsores, classe, test_size=0.25)
+# Transforma o texto para um número
+classe = labelencoder.fit_transform(classe)
+classe_dummy = np_utils.to_categorical(classe)
+# Como são as saídas
+# iris setosa       1 0 0
+# iris virginica    0 1 0
+# iris versicolor   0 0 1
+previsores_treinamento, previsores_teste, classe_treinamento, classe_teste = train_test_split(previsores, classe_dummy, test_size=0.25)
 
 classificador = Sequential()
 # Formula de units: quantidade de entradas (4) + quantidade de saídas (3 - os tipos de saída) e divide por 2
@@ -20,3 +29,14 @@ classificador.compile(optimizer='adam', loss='categorical_crossentropy',
                       metrics=['categorical_accuracy'])
 
 classificador.fit(previsores_treinamento, classe_treinamento, batch_size=10, epochs=1000)
+resultado = classificador.evaluate(previsores_teste, classe_teste)
+previsoes = classificador.predict(previsores_teste)
+previsoes = (previsoes > 0.5)
+
+
+import numpy as np
+classe_teste2 = [np.argmax(t) for t in classe_teste]
+previsoes2 = [np.argmax(t) for t in previsoes]
+
+from sklearn.metrics import confusion_matrix
+matriz = confusion_matrix(previsoes2, classe_teste2)
